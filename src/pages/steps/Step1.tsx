9cdbed56eacc38d08943756201fbd4beabacb233
MixBadGun <1059129006@qq.com>
2024-09-19 23:14:29 +0800

    form add

--- src/pages/steps/Step1.tsx
+++ src/pages/steps/Step1.tsx
@@ -1,121 +1,153 @@
-import { useState } from 'react';
-import { Space, Button, message, Select, Divider, Typography, DatePicker, InputNumber, Flex, theme } from "antd";
-import dayjs from 'dayjs';
-import LogBox from '../../components/LogBox';
-import get from '../../common/api'
-import { address, port } from '../../common/config';
+import { useState } from "react";
+import {
+  Space,
+  Button,
+  message,
+  Select,
+  Divider,
+  Typography,
+  DatePicker,
+  InputNumber,
+  Flex,
+  theme,
+} from "antd";
+import dayjs from "dayjs";
+import LogBox from "../../components/LogBox";
+import get from "../../common/api";
+import { address, port } from "../../common/config";
 
 const { RangePicker } = DatePicker;
 
 const { Title, Paragraph } = Typography;
 
 const SendButton = ({ url }) => {
-    const [messageApi, contextHolder] = message.useMessage();
-    const [buttonState, setButtonState] = useState({
-        danger: false,
-        text: "开始 / 继续获取"});
+  const [messageApi, contextHolder] = message.useMessage();
+  const [buttonState, setButtonState] = useState({
+    danger: false,
+    text: "开始 / 继续获取",
+  });
 
-    const toggleDanger = () => {
-        if( buttonState.danger ){
-            get(`${address}:${port}/backend/get-data/stop`)()
-                .then( _ => { messageApi.success("已终止进程") })
-                .catch( _ => {
-                    { messageApi.error("终止线程时遇到了异常") }
-            })
-        }
-        setButtonState(prevState => ({
-            danger: !prevState.danger,
-            text: prevState.danger ? '开始 / 继续获取' : '终止线程',
-        }));
-    };
-
-    return (
-      <>
-        {contextHolder}
-        {buttonState.danger && <LogBox url={url}/>}
-        <Button style={{marginTop: 12}} type="primary" onClick={toggleDanger} danger={buttonState.danger}>
-            {buttonState.text}
-        </Button>
-      </>
-    );
+  const toggleDanger = () => {
+    if (buttonState.danger) {
+      get(`${address}:${port}/backend/get-data/stop`)()
+        .then((_) => {
+          messageApi.success("已终止进程");
+        })
+        .catch((_) => {
+          {
+            messageApi.error("终止线程时遇到了异常");
+          }
+        });
+    }
+    setButtonState((prevState) => ({
+      danger: !prevState.danger,
+      text: prevState.danger ? "开始 / 继续获取" : "终止线程",
+    }));
   };
 
-function ComText({children}){
-    return <span style={{
+  return (
+    <>
+      {contextHolder}
+      {buttonState.danger && <LogBox url={url} />}
+      <Button
+        style={{ marginTop: 12 }}
+        type="primary"
+        onClick={toggleDanger}
+        danger={buttonState.danger}
+      >
+        {buttonState.text}
+      </Button>
+    </>
+  );
+};
+
+function ComText({ children }) {
+  return (
+    <span
+      style={{
         display: "inline-block",
-        minWidth: "8em"
-    }}>{children}</span>
+        minWidth: "8em",
+      }}
+    >
+      {children}
+    </span>
+  );
 }
 
 export default function MainPage() {
-    const date = new Date();
+  const date = new Date();
 
-    return <>
-        <Title level={2}>数据获取</Title>
-        <Paragraph>获取本次周刊的数据。</Paragraph>
-        <Divider orientation="left">参数设置</Divider>
-        <Space direction="vertical">
-            <Flex vertical={false} align="center">
-                <ComText>获取时间范围：</ComText>
-                <RangePicker
-                    defaultValue={[dayjs(date.setDate(date.getDate() - 13)), dayjs(date.setDate(date.getDate() + 6))]}
-                    disabled
-                /></Flex>
-            <Flex vertical={false} align="center">
-                <ComText>拉取分区代码：</ComText>
-                <Select
-                    mode="tags"
-                    style={{ width: '100%' }}
-                    defaultValue={[26, 126, 22]}
-                    disabled
-                />
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>承认分区代码：</ComText>
-                <Select
-                    mode="tags"
-                    style={{ width: '100%' }}
-                    defaultValue={[26]}
-                    disabled
-                />
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>承认 TAG：</ComText>
-                <Select
-                    mode="tags"
-                    style={{ width: '100%' }}
-                    defaultValue={['音mad', "ytpmv"]}
-                    disabled
-                />
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>拉取视频个数：</ComText>
-                <InputNumber defaultValue={100} disabled/>
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>主榜个数：</ComText>
-                <InputNumber defaultValue={15} disabled/>
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>主榜中断数：</ComText>
-                <InputNumber defaultValue={5} disabled/>
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>副榜个数：</ComText>
-                <InputNumber defaultValue={40} disabled/>
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>间隔时间：</ComText>
-                <InputNumber addonAfter="秒" defaultValue={20} disabled/>
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>主榜最长时间：</ComText>
-                <InputNumber addonAfter="秒" defaultValue={70} disabled/>
-            </Flex>
-        </Space>
-        <Space>
-        </Space>
-        <Divider orientation="left">运行</Divider>
-        <SendButton url={`${address}:${port}/backend/get-data`} />
+  return (
+    <>
+      <Title level={2}>数据获取</Title>
+      <Paragraph>获取本次周刊的数据。</Paragraph>
+      <Divider orientation="left">参数设置</Divider>
+      <Space direction="vertical">
+        <Flex vertical={false} align="center">
+          <ComText>获取时间范围：</ComText>
+          <RangePicker
+            defaultValue={[
+              dayjs(date.setDate(date.getDate() - 13)),
+              dayjs(date.setDate(date.getDate() + 6)),
+            ]}
+            disabled
+          />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>拉取分区代码：</ComText>
+          <Select
+            mode="tags"
+            style={{ width: "100%" }}
+            defaultValue={[26, 126, 22]}
+            disabled
+          />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>承认分区代码：</ComText>
+          <Select
+            mode="tags"
+            style={{ width: "100%" }}
+            defaultValue={[26]}
+            disabled
+          />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>承认 TAG：</ComText>
+          <Select
+            mode="tags"
+            style={{ width: "100%" }}
+            defaultValue={["音mad", "ytpmv"]}
+            disabled
+          />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>拉取视频个数：</ComText>
+          <InputNumber defaultValue={100} disabled />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>主榜个数：</ComText>
+          <InputNumber defaultValue={15} disabled />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>主榜中断数：</ComText>
+          <InputNumber defaultValue={5} disabled />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>副榜个数：</ComText>
+          <InputNumber defaultValue={40} disabled />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>间隔时间：</ComText>
+          <InputNumber addonAfter="秒" defaultValue={20} disabled />
+        </Flex>
+        <Flex vertical={false} align="center">
+          <ComText>主榜最长时间：</ComText>
+          <InputNumber addonAfter="秒" defaultValue={70} disabled />
+        </Flex>
+      </Space>
+      <Space></Space>
+      <Divider orientation="left">运行</Divider>
+      <SendButton url={`${address}:${port}/backend/get-data`} />
     </>
+  );
 }
--- src/pages/steps/Step2.tsx
+++ src/pages/steps/Step2.tsx
@@ -1,113 +1,338 @@
-import { useState } from 'react';
-import { Space, Button, message, Select, Divider, Typography, DatePicker, InputNumber, Flex, theme, Collapse, Input } from "antd";
-import get from '../../common/api'
-import { address, port } from '../../common/config';
+/* eslint-disable react/react-in-jsx-scope */
+import {
+  JSXElementConstructor,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+  useEffect,
+  useState,
+} from "react";
+import {
+  Space,
+  Button,
+  message,
+  Select,
+  Divider,
+  Typography,
+  DatePicker,
+  InputNumber,
+  Flex,
+  theme,
+  Collapse,
+  Input,
+  Form,
+  Row,
+  Col,
+  FormListFieldData,
+} from "antd";
+import { CloseOutlined, ArrowRightOutlined } from "@ant-design/icons";
+import get from "../../common/api";
+import { address, port } from "../../common/config";
+import { JSX } from "react/jsx-runtime";
 
 type pullData = {
-    "ranking": string,
-    "score": string,
-    "aid": string,
-    "bvid": string,
-    "cid": string,
-    "title": string,
-    "uploader": string,
-    "uid": string,
-    "copyright": string,
-    "play": string,
-    "like": string,
-    "coin": string,
-    "star": string,
-    "pubtime": string,
-    "adjust_scale": string,
-    "prescore": string,
-    "part": string,
-    "duration": string,
-    "start_time": string,
-    "full_time": string,
-    "web_prefix": string,
-    "video_src": string,
-    "cover_src": string,
-    "avatar_src": string,
-    "danmaku_src": string,
-    "light_color": string,
-    "dark_color": string,
-    "score_add": string
-}
+  ranking: string;
+  score: string;
+  aid: string;
+  bvid: string;
+  cid: string;
+  title: string;
+  uploader: string;
+  uid: string;
+  copyright: string;
+  play: string;
+  like: string;
+  coin: string;
+  star: string;
+  pubtime: string;
+  adjust_scale: string;
+  prescore: string;
+  part: string;
+  duration: string;
+  start_time: string;
+  full_time: string;
+  web_prefix: string;
+  video_src: string;
+  cover_src: string;
+  avatar_src: string;
+  danmaku_src: string;
+  light_color: string;
+  dark_color: string;
+  score_add: string;
+};
 
 const { Title, Paragraph } = Typography;
 
-function ComText({children}){
-    return <span style={{
+function ComText({ children }) {
+  return (
+    <span
+      style={{
         display: "inline-block",
-        minWidth: "8em"
-    }}>{children}</span>
+        minWidth: "8em",
+      }}
+    >
+      {children}
+    </span>
+  );
 }
 
-const PanelInside = ({ data }: {data: pullData}) => {
-    return <>
-    {/* 后续改为 Form */}
-        <Space direction="vertical">
-            <Flex vertical={false} align="center" gap={10}>
-                <Button type="primary" href={'https://bilibili.com/video/av' + data.aid} target="_blank">av{data.aid}</Button>
-                <Button type="primary" href={'https://bilibili.com/video/' + data.bvid} target="_blank">{data.bvid}</Button>
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>总分数：</ComText>
-                <InputNumber defaultValue={data.score} />
-            </Flex>
-            <Flex vertical={false} align="center">
-                <ComText>额外标签：</ComText>
-                <Input defaultValue={data.score_add} />
-            </Flex>
-        </Space>
+/**
+ * 动态获取表单值，会因值变化而变化
+ */
+const getValue = (field: FormListFieldData, attr: string) => {
+  const form = Form.useFormInstance();
+  return Form.useWatch(["items", field.name, attr], form);
+};
+
+/**
+ * 静态获取表单值，但不重新定义 form
+ */
+function checkValueWithForm(form, field: FormListFieldData, attr: string) {
+  return form.getFieldValue(["items", field.name, attr], form);
+}
+
+/**
+ * 静态获取表单值，仅拉取一次
+ */
+const checkValue = (field: FormListFieldData, attr: string) => {
+  const form = Form.useFormInstance();
+  return form.getFieldValue(["items", field.name, attr]);
+};
+
+const PanelInside = ({ field }) => {
+  const form = Form.useFormInstance();
+
+  const aid = getValue(field, "aid");
+  const bvid = getValue(field, "bvid");
+
+  const scoreChange = () => {
+    form.setFieldValue(
+      ["items", field.name, "score"],
+      checkValueWithForm(form, field, "prescore") *
+        checkValueWithForm(form, field, "adjust_scale")
+    );
+  };
+
+  return (
+    <>
+      <Divider orientation="left">基本信息</Divider>
+      <Flex
+        vertical={false}
+        align="center"
+        gap={10}
+        style={{ marginBottom: 24 }}
+      >
+        <Button
+          type="primary"
+          href={"https://bilibili.com/video/av" + aid}
+          target="_blank"
+        >
+          av{aid}
+        </Button>
+        <Button
+          type="primary"
+          href={"https://bilibili.com/video/" + bvid}
+          target="_blank"
+        >
+          {bvid}
+        </Button>
+      </Flex>
+      <Row gutter={48}>
+        <Col>
+          <Paragraph>上传者：{checkValue(field, "uploader")}</Paragraph>
+          <Paragraph>UID：{checkValue(field, "uid")}</Paragraph>
+          <Paragraph>
+            版权声明：
+            {[1, "1"].includes(checkValue(field, "copyright"))
+              ? "自制"
+              : "转载"}
+          </Paragraph>
+          <Paragraph>视频 CID：{checkValue(field, "cid")}</Paragraph>
+        </Col>
+        <Col>
+          <Paragraph>播放量：{checkValue(field, "play")}</Paragraph>
+          <Paragraph>点赞量：{checkValue(field, "like")}</Paragraph>
+          <Paragraph>硬币量：{checkValue(field, "coin")}</Paragraph>
+          <Paragraph>收藏量：{checkValue(field, "star")}</Paragraph>
+        </Col>
+        <Col>
+          <Paragraph>发布时间：{checkValue(field, "pubtime")}</Paragraph>
+          <Paragraph>视频长度：{checkValue(field, "duration")}</Paragraph>
+          <Paragraph>
+            浅色调：
+            <div
+              style={{
+                display: "inline-block",
+                width: "1.5em",
+                height: "1.5em",
+                marginRight: 5,
+                borderRadius: 2,
+                verticalAlign: "middle",
+                background: "hsl" + checkValue(field, "light_color"),
+              }}
+            ></div>
+            hsl{checkValue(field, "light_color")}
+          </Paragraph>
+          <Paragraph>
+            暗色调：
+            <div
+              style={{
+                display: "inline-block",
+                width: "1.5em",
+                height: "1.5em",
+                marginRight: 5,
+                borderRadius: 2,
+                verticalAlign: "middle",
+                background: "hsl" + checkValue(field, "dark_color"),
+              }}
+            ></div>
+            hsl{checkValue(field, "dark_color")}
+          </Paragraph>
+        </Col>
+      </Row>
+      {/* <Divider orientation="left">排位</Divider> */}
+      {/* <Form.Item label="排位" name={[field.name, "ranking"]}>
+        <InputNumber />
+      </Form.Item> */}
+      <Divider orientation="left">分数</Divider>
+      <Row>
+        <Col>
+          <Row>
+            <Col>
+              <Form.Item label="原始分数" name={[field.name, "prescore"]}>
+                <InputNumber onChange={async () => await scoreChange()} />
+              </Form.Item>
+              <Form.Item label="赋分系数" name={[field.name, "adjust_scale"]}>
+                <InputNumber onChange={async () => await scoreChange()} />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Row align="middle" justify="center" style={{ height: "100%" }}>
+                <ArrowRightOutlined
+                  style={{ fontSize: "3em", margin: "0 1em" }}
+                />
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <Row align="middle" style={{ height: "100%" }}>
+            <Form.Item label="最终分数" name={[field.name, "score"]}>
+              <Input />
+            </Form.Item>
+          </Row>
+        </Col>
+      </Row>
+      <Divider orientation="left">配置</Divider>
+      <Form.Item label="视频分 P" name={[field.name, "part"]}>
+        <InputNumber />
+      </Form.Item>
+      <Form.Item label="截取起点" name={[field.name, "start_time"]}>
+        <InputNumber addonAfter="秒" />
+      </Form.Item>
+      <Form.Item label="截取时长" name={[field.name, "full_time"]}>
+        <InputNumber addonAfter="秒" />
+      </Form.Item>
+      <Form.Item label="额外标签" name={[field.name, "score_add"]}>
+        <Input />
+      </Form.Item>
     </>
-}
+  );
+};
 
+const ItemList = ({ field, remove }) => {
+  const form = Form.useFormInstance();
+
+  return (
+    <>
+      <Collapse
+        size="large"
+        items={[
+          {
+            key: "1",
+            label: (
+              <Flex align="center">
+                <span
+                  style={{
+                    display: "inline-block",
+                    fontSize: "1.5em",
+                    fontWeight: "bold",
+                    minWidth: "2em",
+                  }}
+                >
+                  {field.name + 1}
+                </span>
+                {checkValue(field, "title")}
+                <Button
+                  danger
+                  style={{
+                    position: "absolute",
+                    right: 55,
+                  }}
+                  onClick={async () => await remove(field.name)}
+                >
+                  删除
+                </Button>
+              </Flex>
+            ),
+            children: <PanelInside field={field} />,
+          },
+        ]}
+        expandIconPosition="end"
+      />
+    </>
+  );
+};
 const DataBox = () => {
-    const [listData, setListData] = useState<[pullData]>([{ranking: "1"}]);
+  const [form] = Form.useForm();
 
-    const itemList = listData.map(item => {
-        if(item.ranking <= 55){
-        return <Collapse
-                    size='large'
-                    key={item.ranking}
-                    items={[{ key: '1',
-                            label: <Flex align='center'>
-                                        <span style={
-                                            {
-                                                display: "inline-block",
-                                                fontSize: "1.5em",
-                                                fontWeight: "bold",
-                                                minWidth: "3em"
-                                            }
-                                        }>{item.ranking}</span>
-                                        {item.title}
-                                    </Flex>,
-                            children: <PanelInside data={item} />}]}
-                    expandIconPosition='end'
-                    />
-                }
-    })
+  const [listData, setListData] = useState<[pullData]>([]);
+  form.setFieldValue("items", listData);
 
+  useEffect(() => {
     get(`${address}:${port}/backend/pull-data`)().then((data) => {
-        setListData(data);
-    })
-    return <>
-        <Space direction="vertical" style={{width: "100%"}}>
-            { itemList }
-        </Space>
+      setListData(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Form form={form}>
+        <Form.List name="items">
+          {(fields, { add, remove }) => (
+            <Space direction="vertical" style={{ width: "100%" }}>
+              {fields.map((field) => {
+                if (field.key < 10) {
+                  return (
+                    <ItemList key={field.key} field={field} remove={remove} />
+                  );
+                }
+              })}
+            </Space>
+          )}
+        </Form.List>
+        <Form.Item noStyle shouldUpdate>
+          {() => (
+            <Typography>
+              <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
+            </Typography>
+          )}
+        </Form.Item>
+      </Form>
     </>
-}
+  );
+};
 
 export default function MainPage() {
-    return <>
-        <Title level={2}>审核 / 编辑数据</Title>
-        <Paragraph>获取数据后，可以在这里审核并编辑数据。</Paragraph>
-        <Divider orientation="left">数据列表</Divider>
-        <DataBox />
-        <Space>
-        </Space>
-        <Divider orientation="left">数据提交</Divider>
-        {/* <SendButton url={`${address}:${port}/backend/get-data`} /> */}
+  return (
+    <>
+      <Title level={2}>审核 / 编辑数据</Title>
+      <Paragraph>获取数据后，可以在这里审核并编辑数据。</Paragraph>
+      <Divider orientation="left">数据列表</Divider>
+      <DataBox />
+      <Space></Space>
+      <Divider orientation="left">数据提交</Divider>
+      {/* <SendButton url={`${address}:${port}/backend/get-data`} /> */}
     </>
+  );
 }
